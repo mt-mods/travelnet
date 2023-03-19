306a5643ae4538e8604063e73cc843a0de1dcb7b
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-03-19 16:09:17 +0100

    add support for custom privs

--- privs.lua
+++ privs.lua
@@ -1,11 +1,17 @@
 local S = minetest.get_translator("travelnet")
 
-minetest.register_privilege("travelnet_attach", {
-	description = S("allows to attach travelnet boxes to travelnets of other players"),
-	give_to_singleplayer = false
-})
+minetest.register_on_mods_loaded(function()
+	if not minetest.registered_privileges[travelnet.attach_priv] then
+		minetest.register_privilege(travelnet.attach_priv, {
+			description = S("allows to attach travelnet boxes to travelnets of other players"),
+			give_to_singleplayer = false
+		})
+	end
 
-minetest.register_privilege("travelnet_remove", {
-	description = S("allows to dig travelnet boxes which belog to nets of other players"),
-	give_to_singleplayer = false
-})
+	if not minetest.registered_privileges[travelnet.remove_priv] then
+		minetest.register_privilege(travelnet.attach_priv, {
+			description = S("allows to dig travelnet boxes which belog to nets of other players"),
+			give_to_singleplayer = false
+		})
+	end
+end)
